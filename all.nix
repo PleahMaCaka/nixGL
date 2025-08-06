5c4bd803d148c560763279f5355a515e3904374e
PleahMaCaka <pleahmacaka@gmail.com>
2025-08-06 16:44:04 +0900

    feat!: unfree fix

--- all.nix
+++ all.nix
@@ -1,6 +1,6 @@
-let
-  pkgs = import ./nixpkgs.nix { config = { allowUnfree = true; }; };
+{ pkgs }:
 
+let
   pure = pkgs.recurseIntoAttrs (pkgs.callPackage ./nixGL.nix {
     nvidiaVersion = "440.82";
     nvidiaHash = "edd415acf2f75a659e0f3b4f27c1fab770cf21614e84a18152d94f0d004a758e";
